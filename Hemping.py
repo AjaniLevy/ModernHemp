c78e2426ff28fd5f0281beb1b0c466e422675f56
Ajani Levy <AjaniLevy@gmail.com>
2022-05-05 19:24:47 -0400

    Not Visible/Legible B4

---
+++ Hemping.py
@@ -0,0 +1,12 @@
+
+
+Hemps = {
+        "Green" : 2,
+        "Blue" : 3,
+        "Yellow and Indigo" : 4,
+        "Berry-Sage" : 6,
+        "Crispy-Orange" : 5,
+        "Nasty-Vomitious" : 12,
+        "Mellow Yellow" : 9, 
+        "Killer-Barf" : 8
+        }
